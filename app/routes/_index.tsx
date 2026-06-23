7f45ffb0a3bf8fc822238a52b4bb0fbb9b7317da
krtoca <krtoca@gmail.com>
2026-06-23 10:19:28 -0400

    Redirect root to embedded app

---
+++ app/routes/_index.tsx
@@ -0,0 +1,11 @@
+﻿import type { LoaderFunctionArgs } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const url = new URL(request.url);
+  return redirect(`/app${url.search}`);
+}
+
+export default function Index() {
+  return null;
+}
